c6c004fab46d3a6759e1dcf1edb74694df7f8d6c
Balaji <hellobeekay@gmail.com>
2026-04-03 17:20:20 +0530

    Initial Commit

---
+++ clear atticTests/clear_atticTests.swift
@@ -0,0 +1,18 @@
+//
+//  clear_atticTests.swift
+//  clear atticTests
+//
+//  Created by Balaji K on 03/04/26.
+//
+
+import Testing
+
+struct clear_atticTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
